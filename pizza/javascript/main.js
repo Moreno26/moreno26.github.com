f7897980b3ec8ed1d2088bd2693af02069dcb8df
Moreno26 <mamch.94@gmail.com>
2020-05-07 11:04:21 -0500

    Cambio de direccionamiento

--- pizza/javascript/main.js
+++ pizza/javascript/main.js
@@ -1,62 +1,18 @@
-/*console.log("hola mundo desde un archivo extrno de java script");
+let links = document.querySelectorAll(".close");
 
-// DOM 
-// querySelector    /       querySelectorAll
-let links = document.querySelectorAll("a");
+links.forEach(function(link){
+    link.addEventListener("click", function(ev){
+        ev.preventDefault();
+        let content = document.querySelector('.content');
+        content.classList.remove("fadeInDown");
+        content.classList.remove("animated");
+        content.classList.add("fadeOutUp");
+        content.classList.add("animated");
 
-links.forEach(function(link) {
-    console.log(link); 
-    
-});
+        setTimeout(function(){
+            location.href = "/pizza";
+        },600)
 
-let celdas = document.querySelectorAll("td");
-    
-celdas.forEach(function(td){
-    td.addEventListener('click',function(){
-        console.log("Click")
+        return false;
     })
 })
-
-
-let closes = document.querySelectorAll(".close");
-
-closes.forEach(function(close){
-    close.addEventListener("Click",function(){
-        console.log("Click")
-    })
-});
-
-let iconos = document.querySelectorAll("i");
-
-    iconos.forEach(function(icono){
-        icono.classList.remove("fa-star-o");
-      
-    })
-
-    let links = document.querySelectorAll(".close");
-
-    links.forEach(function(link){   
-
-        link.addEventListener("click",function(ev){
-            ev.preventDefault();
-            
-            
-           
-            return false;   
-            let content = document.querySelector(".content");
-
-            //quitamos las clases de animacion que ya tienen
-             content.classList.remove("fadeInDown");    
-             content.classList.remove("animated");
-            
-            //agregamos clases para animar la   salida con FadeOutuP
-            content.classList.add("fadeOutUp");
-            content.classList.add("animated");
-            setTimeout(function(){
-                location.href= "/";
-            },1000);
-        
-        })
-    })
-  
-    */
